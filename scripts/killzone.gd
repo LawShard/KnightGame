7f878576de87d4f694f4967267feafe5515f3566
LawShard <franco.stival@gmail.com>
2024-05-09 21:54:05 +0200

    Version 0.1.2

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
 
 @onready var timer = $Timer
 
 func _on_body_entered(body): #Señal detecta colision con Killzone
 	print("You Died!")
+	audio_stream_player_2d.play()
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start() #Inicia un contador
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D
-@onready var player_animated_sprite_2d = $PlayerAnimatedSprite2D
-@onready var sword_animated_sprite_2d = $PlayerAnimatedSprite2D/Sword/SwordAnimatedSprite2D
+@onready var player_animated_sprite_2d = $Marker2D/PlayerAnimatedSprite2D
+@onready var sword = $Marker2D/PlayerAnimatedSprite2D/Sword
+@onready var sword_timer = $Marker2D/PlayerAnimatedSprite2D/Sword/SwordTimer
 @onready var coyote_timer = $CoyoteTimer
-@onready var sword = $PlayerAnimatedSprite2D/Sword
+@onready var sword_animated_sprite_2d = $Marker2D/PlayerAnimatedSprite2D/Sword/SwordAnimatedSprite2D
+@onready var marker_2d = $Marker2D
+@onready var colisionEnemigo = $Killzone/CollisionShape2D
+@onready var jump_audio = $Sonidos/JumpAudio
 
 
+var shine = 10
 var coyote_time = 0.3
 var can_jump = false
 
@@ -13,9 +18,10 @@ const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 func _ready():
-	sword_animated_sprite_2d.rotation = 0
-	
+	sword_timer.start()
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -29,6 +35,7 @@ func _physics_process(delta):
 	if can_jump:
 		if Input.is_action_just_pressed("Jump"):
 			velocity.y = JUMP_VELOCITY
+			jump_audio.play()
 
 
 	# Pilla la direccion, -1, 0, 1
@@ -36,11 +43,9 @@ func _physics_process(delta):
 	
 	#Cambiar Sprite
 	if direction > 0:
-		player_animated_sprite_2d.flip_h = false
-		sword_animated_sprite_2d.rotation = 0
+		marker_2d.scale.x = 1
 	elif direction < 0:
-		player_animated_sprite_2d.flip_h = true
-		sword_animated_sprite_2d.rotation *= -1
+		marker_2d.scale.x =- 1
 
 	#Animaciones
 	if is_on_floor():
@@ -64,3 +69,16 @@ func _physics_process(delta):
 func _on_coyote_timer_timeout():
 	can_jump = false
 	pass # Replace with function body.
+
+
+func _on_sword_timer_timeout():
+	sword_animated_sprite_2d.play("Shinning")
+
+
+func _on_sword_animated_sprite_2d_animation_finished():
+	sword_animated_sprite_2d.play("Idle")
+	sword_timer.start(shine)
+
+
+
+
